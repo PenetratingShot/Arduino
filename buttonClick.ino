431283b87012af6fd36b432d57c267ad6c0a1091
Shreyas Lad <shadowtemplates@gmail.com>
2017-03-29 10:35:51 -0700

    Boom

--- buttonClick.ino
+++ buttonClick.ino
@@ -1,1 +1,29 @@
+/*
+  * Push button get LED
+  * Not Rocket Mortgage
+*/
 
+const int buttonPinCom = 3; // wht pin the button is connected to
+const int ledPin = 9; // conneted to pin 9
+
+int buttonPowa = 0; // 0 = button isn't pressed
+
+void setup() {
+  // set led as OUTPUT
+  pinMode(ledPin, OUTPUT);
+  // set button as INPUT
+  pinMode(buttonPinCom, INPUT);
+}
+
+void loop() {
+  // detect button push
+  digitalRead(buttonPowa);
+  
+  // what to do if button is pressed
+  if (buttonPowa == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
